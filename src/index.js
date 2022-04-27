d7ab30e4fcdd5f20b7e9e98bf233da83a4d128b1
kamal heydari <kamal3heydari@gmail.com>
2022-04-27 22:59:14 +0430

    import fonts & global styles

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+//? fonts
+import "@fontsource/audiowide";
+
+import "@fontsource/poppins";
+import "@fontsource/poppins/500.css";
+
+import "@fontsource/oswald/500.css";
+import "@fontsource/oswald/700.css";
+
+//? global style
+import GlobalStyle from "./styles/GlobalStyle";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>
 );
