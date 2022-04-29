d0d495826b9e4739c759d3d09d84faddd8a4063b
kamal heydari <kamal3heydari@gmail.com>
2022-04-29 14:09:02 +0430

    feat sidebar

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,5 +1,22 @@
-const Sidebar = () => {
-  return <div>sidebar</div>;
+import { Logo } from "../../assets/images";
+import { Icons, NavItems } from "..";
+import SidebarStyles from "./Sidebar.styles";
+
+const Sidebar = ({ isSidebarOpen, setIsSidebarOpen }) => {
+  return (
+    <SidebarStyles isSidebarOpen={isSidebarOpen}>
+      <div className="sidebar__overlay"onClick={() => setIsSidebarOpen(false)}/>
+      <div className='sidebar__header'>
+        <img src={Logo} alt='Game - x logo' />
+        <Icons.MenuCloseStyled onClick={() => setIsSidebarOpen(false)} />
+      </div>
+      <NavItems className='sidebar__nav' />
+      <div className='sidebar__social'>
+        <Icons.TwitterStyled /> <Icons.InstagramStyled /> <Icons.GithubStyled />
+        <Icons.LinkedinStyled />
+      </div>
+    </SidebarStyles>
+  );
 };
 
 export default Sidebar;
---
+++ src/components/sidebar/Sidebar.styles.js
@@ -0,0 +1,65 @@
+import styled from "styled-components";
+import Screen from "../../styles/Screen";
+
+const SidebarStyles = styled.div`
+  background-color: var(--raisin-black-2);
+  width: min(100%, 430px);
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  right: ${(props) => (props.isSidebarOpen ? "0" : "-100%")};
+  transition: var(--transition-3);
+  box-shadow: 0 3px 27px hsla(0, 0%, 0%, 0.5);
+
+  ${Screen.lg`
+    display:none
+  `}
+
+  .sidebar__header {
+    padding: 1rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    border-bottom: 1px solid var(--gray);
+  }
+  .sidebar__nav {
+    display: block;
+    margin-bottom: 3rem;
+    a {
+      display: inline-block;
+      width: 100%;
+      border-bottom: 1px solid var(--gray);
+      padding: 0.5rem 0.8rem;
+      color: var(--platinum);
+      font-weight: var(--fw-500);
+      transition: var(--transition-2);
+
+      &:hover {
+        padding-left: 1rem;
+        color: var(--orange);
+      }
+    }
+  }
+  .sidebar__social {
+    display: flex;
+    justify-content: center;
+    gap: 0.8rem;
+  }
+  .sidebar__overlay {
+    position: fixed;
+    inset: 0;
+    pointer-events: none;
+    z-index: -1;
+    transition: var(--transition-2);
+
+    ${(props) =>
+      props.isSidebarOpen &&
+      `
+      background: hsla(0, 0%, 0%, 0.7);
+      pointer-events: all;
+      transition: var(--transition-3);
+  `};
+  }
+`;
+
+export default SidebarStyles;
